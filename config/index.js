1cf4a5bdcd5ee853f33ca70a5a70795c208a12b6
yinmei <1102316727@qq.com>
2018-03-28 06:47:37 +0800

    change the path of build

--- config/index.js
+++ config/index.js
@@ -42,7 +42,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'vue-resumer/dist/static',
     assetsPublicPath: '/',
 
     /**
